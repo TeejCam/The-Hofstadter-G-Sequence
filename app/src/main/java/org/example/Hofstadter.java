60614322e8ea73d4107f057075407c58db3fada1
TeejCam <tjcamalan@gmail.com>
2025-01-30 21:38:41 -0600

    Added necessary implementation

--- app/src/main/java/org/example/Hofstadter.java
+++ app/src/main/java/org/example/Hofstadter.java
@@ -6,6 +6,15 @@ public class Hofstadter {
   // G(n) = n - G(G(n-1))
   // G(0) = 0
   public Integer gSequence(Integer index) {
-    return 0;
+    if (index == 0){
+      return 0;
+    } else {
+      return index - gSequence(gSequence(index - 1));
+    }
+  }
+
+  public static void main(String[] args)
+  {
+    
   }
 }
--- app/src/test/java/org/example/HofstadterTest.java
+++ app/src/test/java/org/example/HofstadterTest.java
@@ -2,11 +2,74 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
-class HofstadterTest {
+public class HofstadterTest {
+  Hofstadter h;
+
+  @BeforeEach
+  void setUp() {
+    h = new Hofstadter();
+  }
+
   @Test
-  void itReturnsTheZerothElementInTheSequence() {
-    Hofstadter h = new Hofstadter();
+  void testZeroethElement() {
     assertEquals(0, h.gSequence(0));
   }
+
+  @Test 
+  void testFirstElement() {
+    assertEquals(1, h.gSequence(1));
+  }
+
+  @Test 
+  void testSecondElement() {
+    assertEquals(1, h.gSequence(2));
+  }
+
+  @Test 
+  void testThirdElement() {
+    assertEquals(2, h.gSequence(3));
+  }
+
+  @Test 
+  void testFourthElement() {
+    assertEquals(3, h.gSequence(4));
+  }
+
+  @Test 
+  void testFifthElement() {
+    assertEquals(3, h.gSequence(5));
+  }
+
+  @Test 
+  void testSixthElement() {
+    assertEquals(4, h.gSequence(6));
+  }
+
+  @Test 
+  void testSeventhElement() {
+    assertEquals(4, h.gSequence(7));
+  }
+
+  @Test 
+  void testEighthElement() {
+    assertEquals(5, h.gSequence(8));
+  }
+
+  @Test 
+  void testNinthElement() {
+    assertEquals(6, h.gSequence(9));
+  }
+
+  @Test 
+  void testTenthElement() {
+    assertEquals(6, h.gSequence(10));
+  }
+
+  @Test 
+  void testEleventhElement() {
+    assertEquals(7, h.gSequence(11));
+  }
+
 }
